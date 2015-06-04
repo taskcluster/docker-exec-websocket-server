2d9b13328cdf5ef2cae2549b511bed55bf215450
Edgar Chen <eggylv999@gmail.com>
2015-06-04 13:49:48 -0700

    added exit code test

--- test/test.js
+++ test/test.js
@@ -1,23 +1,25 @@
 suite('trying server', () => {
+  var debug = require('debug')('docker-exec-websocket-server:test');
+  var ws = require('ws');
+  var Server = require('../lib/server.js');
+  var slugid = require('slugid');
+  var path = require('path');
+  var assert = require('assert');
+
+  var randpath = '/'+slugid.v4();
+
+  var server = new Server({
+    container: 'servertest',
+    port: 8080,
+    path: randpath,
+  });
+  assert(server,'server required!');
   test('cat', () => {
-    var debug = require('debug')('docker-exec-websocket-server:test');
-    var ws = require('ws');
-    var Server = require('../lib/server.js');
-    var slugid = require('slugid');
-    var path = require('path');
-    var assert = require('assert');
-
-    var randpath = '/'+slugid.v4();
-
-    var server = new Server({
-      container: 'servertest',
-      port: 8080,
-      path: randpath,
-    });
-    assert(server,'server required!');
-
     var socket = new ws('ws://localhost:8080'+randpath);
     assert(socket,'socket connection required!');
+    socket.on('open',() => {
+      socket.send('cat');
+    });
 
     socket.once('message',(message) => {
       debug(message);
@@ -35,8 +37,19 @@ suite('trying server', () => {
         assert(buf.compare(message) === 0);
       })
     });
+  });
+  test('exit code', () => {
+    var socket = new ws('ws://localhost:8080'+randpath);
+    assert(socket,'socket connection required!');
     socket.on('open',() => {
-      socket.send('cat');
+      socket.send('/bin/bash');
+    });
+    socket.once('message', () => {
+      socket.on('message', (message) => {
+        assert(message[0] === 0x64, 'message code wrong');
+        assert(message.readUInt32LE(1) === 5, 'exit code wrong');
+      });
+      socket.send('exit 5\r\n');
     });
   });
 });
