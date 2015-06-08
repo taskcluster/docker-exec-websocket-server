c0a0659fb00cb649d7c09a41ad2158d3f787228b
Edgar Chen <eggylv999@gmail.com>
2015-06-07 19:30:20 -0700

    removing unnecessary packages

--- test/client.js
+++ test/client.js
@@ -1,7 +1,5 @@
 suite('trying client', () => {
   var debug = require('debug')('docker-exec-websocket-server:test:client');
-  var through = require('through');
-  var msgcode = require('../lib/messagecodes.js');
   var DockerClient = require('../lib/client.js');
   var DockerServer = require('../lib/server.js');
   var base = require('taskcluster-base');
