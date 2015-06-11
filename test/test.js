4f0aef939cb6877e1a7853c23af22045e134a181
Edgar Chen <eggylv999@gmail.com>
2015-06-11 12:11:22 -0700

    moving around test file

--- test/test.js
+++ test/test.js
@@ -1,79 +1,112 @@
-suite('trying server', () => {
-  var debug = require('debug')('docker-exec-websocket-server:test');
-  var ws = require('ws');
-  var Server = require('../lib/server.js');
-  var slugid = require('slugid');
-  var path = require('path');
-  var assert = require('assert');
-  var url = require('url');
+suite('trying client', () => {
+  var debug = require('debug')('docker-exec-websocket-server:test:testclient');
+  var DockerClient = require('../lib/client.js');
+  var DockerServer = require('../lib/server.js');
   var base = require('taskcluster-base');
-  var msgcode = require('../lib/messagecodes');
+  var assert = require('assert');
+  var Promise = require('promise');
 
-  var randpath = '/' + slugid.v4();
-
-  var server = new Server({
+  var server = new DockerServer({
+    port: 8081,
     containerId: 'servertest',
-    port: 8080,
-    path: randpath,
-    log: false,
+    path: '/a',
+    log: true,
+    maxSessions: 1,
   });
-  assert(server, 'server required!');
-  test('cat', async () => {
-    var socket = new ws(url.format({
-      protocol: 'ws',
-      slashes: true,
-      host: 'localhost:8080',
-      pathname: randpath,
-      query: {
-        command: encodeURIComponent(JSON.stringify(['cat', '-E'])),
-        tty: false,
-      },
-    }));
-    assert(socket, 'socket connection required!');
 
+  test('cat', async () => {
+    var client = new DockerClient({
+      hostname: 'localhost',
+      port: 8081,
+      pathname: 'a',
+      tty: false,
+      command: ['cat', '-E'],
+    });
+    await client.execute();
+    var buf1 = new Buffer([0xfa, 0xff, 0x0a]);
+    client.stdin.write(buf1);
     var passed = false;
-    socket.once('message', (message) => {
-      debug(message);
-      var buf1 = new Buffer([0xfa, 0xff, 0x0a]);
-      socket.send(buf1, {binary: true, mask: true});
-      socket.on('message', (message) => {
-        //looks something like messagecode 0xfa 0xff $(-E option) 0x0a
-        var buf = new Buffer([0x01, 0xfa, 0xff, 0x24, 0x0a]); 
-        assert(buf.compare(message) === 0, 'message wrong!');
-        passed = true;
-      });
+    client.stdout.on('data', (message) => {
+      var buf = new Buffer([0xfa, 0xff, 0x24, 0x0a]); //looks something like 0xfa 0xff $(-E option) 0x0a
+      assert(buf.compare(message) === 0, 'message wrong!');
+      passed = true;
     });
     await base.testing.poll(async () => {
-      assert(passed,'message not recieved');
+      assert(passed, 'message not recieved');
     }, 20, 250).then(() => {
-      debug('successful');
+      client.close();
     }, err => {throw err; });
   });
+
   test('exit code', async () => {
-    var socket = new ws(url.format({
-      protocol: 'ws',
-      slashes: true,
-      host: 'localhost:8080',
-      pathname: randpath,
-      query: {
-        command: encodeURIComponent(JSON.stringify(['/bin/bash'])),
-        tty: true,
-      },
-    }));
-    assert(socket, 'socket connection required!');
+    var client = new DockerClient({
+      hostname: 'localhost',
+      port: 8081,
+      pathname: 'a',
+      tty: true,
+      command: ['/bin/bash'],
+    });
+    await client.execute();
+    client.stdin.write('exit 9\n');
     var passed = false;
-    socket.once('message', () => {
-      socket.on('message', (message) => {
-        if (message[0] === msgcode.stopped && message.readInt8(1) === 9) {
-          passed = true;
-        }
-      });
-      socket.send('exit 9\n', {binary: true});
+    client.on('exit', (code) => {
+      assert(code === 9, 'message wrong!');
+      passed = true;
     });
     await base.testing.poll(async () => {
-      assert(passed, 'timeout');
+      assert(passed, 'exit message not recieved');
     }, 20, 250).then(() => {
-      debug('successful');
     }, err => {throw err; });
+  });
+
+  test('server pause', async (done) => {
+    var client = new DockerClient({
+      hostname: 'localhost',
+      port: 8081,
+      pathname: 'a',
+      tty: false,
+      command: ['cat'],
+    });
+    await client.execute();
+    client.pause();
+    var paused = true;
+    client.stdin.write('hello\n');
+    client.stdout.on('data', (message) => {
+      assert(!paused, 'message recieved too early');
+      assert(message.toString() =='hello\n', 'message recieved was incorrect');
+      client.close();
+      done();
+    });
+    setTimeout(() => {
+      paused = false;
+      client.resume();
+      setTimeout(() => {
+        throw new Error('message too slow');
+      }, 1000);
+    }, 1000);
+  });
+
+  test('connection limit', async (done) => {
+    var client = new DockerClient({
+      hostname: 'localhost',
+      port: 8081,
+      pathname: 'a',
+      tty: false,
+      command: ['cat'],
+    });
+    var client2 = new DockerClient({
+      hostname: 'localhost',
+      port: 8081,
+      pathname: 'a',
+      tty: false,
+      command: ['cat'],
+    });
+    await client.execute();
+    client2.on('error', (errorStr) => {
+      assert(errorStr.toString() === 'Too many sessions active!');
+      client.close();
+      done();
+    });
+    client2.execute();
   });
 });
