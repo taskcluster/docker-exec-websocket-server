12dddccf27071573f03ef81e8c470871facf4bcd
Edgar Chen <eggylv999@gmail.com>
2015-06-15 14:46:41 -0700

    added server option support

--- test/test.js
+++ test/test.js
@@ -1,19 +1,48 @@
-suite('trying client', () => {
+suite('trying client', (done) => {
   var debug = require('debug')('docker-exec-websocket-server:test:testclient');
   var DockerClient = require('../lib/client.js');
   var DockerServer = require('../lib/server.js');
   var base = require('taskcluster-base');
   var assert = require('assert');
+  var http = require('http');
 
-  var server = new DockerServer({
+  var serverPort = new DockerServer({
     port: 8081,
     containerId: 'servertest',
     path: '/a',
   });
 
-  test('cat', async () => {
+  test('cat on port', async () => {
     var client = new DockerClient({
       url: 'ws://localhost:8081/a',
+      tty: false,
+      command: ['cat', '-E'],
+    });
+    await client.execute();
+    var buf1 = new Uint8Array([0xfa, 0xff, 0x0a]);
+    client.stdin.write(buf1);
+    var passed = false;
+    client.stdout.on('data', (message) => {
+      var buf = new Buffer([0xfa, 0xff, 0x24, 0x0a]); //0x24 is $ from the -E option
+      assert(buf.compare(message) === 0, 'message wrong!');
+      passed = true;
+    });
+    await base.testing.poll(async () => {
+      assert(passed, 'message not recieved');
+    }, 20, 250).then(() => {
+      client.close();
+    }, err => {throw err; });
+  });
+
+  test('cat on server', async () => {
+    var httpServ = http.createServer().listen(8083);
+    var serverServer = new DockerServer({
+      containerId: 'servertest',
+      server: httpServ,
+      path: '/a',
+    });
+    var client = new DockerClient({
+      url: 'ws://localhost:8083/a',
       tty: false,
       command: ['cat', '-E'],
     });
