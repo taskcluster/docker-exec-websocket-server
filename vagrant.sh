188579f85fb99ae613f22c9bc7f50192463d870d
John Whitlock <jwhitlock@mozilla.com>
2022-02-02 15:23:37 -0600

    Remove Vagrant files

--- vagrant.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -vex
-sudo apt-get update
-sudo apt-get install -y build-essential lxc libfontconfig1
-
-# Install node
-export NODE_VERSION=v0.12.2
-cd /usr/local/ && \
-curl https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.gz | tar -xz --strip-components 1 && \
-node -v
